48002408cad4f5e2010714899f335de6ea561baa
Saleh Alatas <msbalatas26@binb.idserve.net>
2026-05-27 11:19:27 +0700

    added new xcode projcet

---
+++ BeTidy/BeTidy/BeTidyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeTidyApp.swift
+//  BeTidy
+//
+//  Created by Muhammad Saleh Bagir Alatas on 26/05/26.
+//
+
+import SwiftUI
+
+@main
+struct BeTidyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

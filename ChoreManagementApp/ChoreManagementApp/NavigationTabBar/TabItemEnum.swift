e02d5848f0c44bccd9b4d19707d2176765e4d2e0
freethought101 <salehbageer@gmail.com>
2026-05-28 12:57:19 +0700

    feat: create a custom navigation tab bar, tab item, and tab item enum
    selectedTabItem to switch the current page, each page is its own unique value in TabItemEnum. This is designed to use switch conditional statement in the main view.

---
+++ ChoreManagementApp/ChoreManagementApp/NavigationTabBar/TabItemEnum.swift
@@ -0,0 +1,14 @@
+//
+//  TabItem.swift
+//  ChoreManagementApp
+//
+//  Created by Muhammad Saleh Bagir Alatas on 28/05/26.
+//
+
+// Enum for each tab item, each value for each page
+
+import Foundation
+
+enum TabItemEnum {
+    case home, tasks, schedule, profile
+}

b78ca2851d0a789cf4daf13d8a2762bf765f1a80
freethought101 <salehbageer@gmail.com>
2026-05-28 10:13:38 +0700

    feat: create xcode project
    Name: ChoreManagementApp
    Testing: none
    Storage: none
    Team: personal account

---
+++ ChoreManagementApp/ChoreManagementApp/ChoreManagementAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChoreManagementAppApp.swift
+//  ChoreManagementApp
+//
+//  Created by Muhammad Saleh Bagir Alatas on 28/05/26.
+//
+
+import SwiftUI
+
+@main
+struct ChoreManagementAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChoreManagementApp/ChoreManagementApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChoreManagementApp
+//
+//  Created by Muhammad Saleh Bagir Alatas on 28/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
